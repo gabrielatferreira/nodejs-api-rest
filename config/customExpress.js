50cdc96efdf4facc078bade677bd96c520984219
Gabriela Trindade <byzinha88@hotmail.com>
2021-06-02 19:54:02 -0300

    Finalizado curso de node.js

---
+++ config/customExpress.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const consign = require('consign')
+
+module.exports = () => {
+    const app = express()
+    
+    app.use(express.urlencoded())
+    app.use(express.json())
+
+    consign()
+    .include('controllers')
+    .into(app)
+
+    return app
+  }
+
+
